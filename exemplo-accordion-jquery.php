a5017b848ae15c68fa9d563fee1982757fdb898c
marciorecife <marciosouza1986@gmail.com>
2022-05-30 13:09:31 -0300

    Ajustes na chamada da biblioteca jquery

--- exemplo-accordion-jquery.php
+++ exemplo-accordion-jquery.php
@@ -3,6 +3,10 @@
         <title>Exemplo Accordion em Jquery</title>
 
         <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+
+        <link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel = "stylesheet">
+
+        <script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
         
         <script>  $( function() {
                 $( "#accordion" ).accordion();
